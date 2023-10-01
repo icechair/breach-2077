2b4605a6550351a99ca0359c6bc31d95b3b53788
Martin Gabel <martin.gabel@icechair.de>
2023-10-01 22:19:04 +0200

    opencv, loading spinner

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,1 +1,18 @@
 // place files you want to import through the `$lib` alias in this folder.
+import { browser } from "$app/environment";
+
+/** @return {Promise<void>} */
+export function waitForCv() {
+  return new Promise((resolve, reject) => {
+    if (!browser) {
+      return reject();
+    }
+
+    let timeout = setTimeout(() => {
+      if (window.hasOwnProperty("cv")) {
+        clearTimeout(timeout);
+        resolve();
+      }
+    }, 100);
+  });
+}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,59 @@
 <script>
+  import { waitForCv } from "$lib";
+  import Spinner from "./spinner.svelte";
+  /** @type {HTMLVideoElement} */
+  let video;
+  /** @type {MediaStream} */
+  let stream;
+  /** @type {HTMLCanvasElement} */
+  let canvas;
+
+  let cv_available = waitForCv();
+
+  async function select_camera() {
+    stream = await navigator.mediaDevices.getUserMedia({
+      video: { facingMode: "environment" },
+    });
+    video.srcObject = stream;
+    video.play();
+
+    let cap = new cv.VideoCapture(video);
+    let frame = new cv.Mat(video.height, video.width, cv.CV_8UC4);
+    cap.read(frame);
+
+    requestAnimationFrame(process_video);
+  }
+
+  /** @type {FrameRequestCallback}*/
+  function process_video(time) {
+    requestAnimationFrame(process_video);
+  }
 </script>
 
 <h1>Cyberpunk 2077 Breach Solver</h1>
-<button>Help</button>
+
+<div class="vbox">
+  {#await cv_available}
+    <Spinner />
+  {:then _}
+    {#if !stream}
+      <button on:click={select_camera}>Start</button>
+    {/if}
+    <video bind:this={video} class="back-buffer">
+      <track kind="captions" />
+    </video>
+    <canvas bind:this={canvas} class="front-buffer" />
+  {/await}
+</div>
+
+<style>
+  .vbox {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+  video {
+    max-width: 100vw;
+    height: 50vh;
+  }
+</style>
